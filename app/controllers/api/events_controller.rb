d72d94da922c35734ee5b34f82e8f2f048ee5030
Matheus Nunes <matheus.o.nunes@gmail.com>
2020-06-16 08:55:41 -0300

    Add events API

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,17 @@
+class Api::EventsController < ApplicationController
+  def create
+    event = Event.create(event_params)
+
+    if event.persisted?
+      render json: event
+    else
+      render json: event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.permit(:name, options: {})
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -27,7 +28,6 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -38,6 +38,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include RequestHelpers, type: :request
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
