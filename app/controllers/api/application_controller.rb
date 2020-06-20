40e5831a6b91ede23307ba2733f52e5bf872a54c
Matheus Nunes <matheus.o.nunes@gmail.com>
2020-06-20 01:43:37 -0300

    Ignore authenticity token

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,4 +1,6 @@
 class Api::ApplicationController < ActionController::API
+  skip_before_action :verify_authenticity_token
+
   def require_params(*required_params)
     required_params = required_params.map(&:to_s)
     return true if (params.keys & required_params).size == required_params.size
