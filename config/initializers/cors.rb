15c4913494cc476a5164d9017157611c18823db4
Matheus Nunes <matheus.o.nunes@gmail.com>
2020-06-20 01:56:36 -0300

    Expose CORS header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,8 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    resource '*', headers: :any, methods: %i[get post put patch delete options head],
+                  credentials: false, expose: ['Access-Control-Allow-Origin']
   end
 end
